31ef4262569c452474f48831051a498fbeacb7fe
RYBOC <ryboc00@RYBOC.local>
2016-09-22 12:53:52 +0900

    名前を追加しました

--- Leftter/iOS/ViewController.designer.cs
+++ Leftter/iOS/ViewController.designer.cs
@@ -14,8 +14,43 @@ namespace Leftter.iOS
     [Register ("ViewController")]
     partial class ViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton getButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView logTextView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton sendButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField sendText { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (getButton != null) {
+                getButton.Dispose ();
+                getButton = null;
+            }
+
+            if (logTextView != null) {
+                logTextView.Dispose ();
+                logTextView = null;
+            }
+
+            if (sendButton != null) {
+                sendButton.Dispose ();
+                sendButton = null;
+            }
+
+            if (sendText != null) {
+                sendText.Dispose ();
+                sendText = null;
+            }
         }
     }
 }
